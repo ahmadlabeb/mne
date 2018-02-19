b68cbe25678c755a12f83607b3a6073c492afe18
Ahmad Labeb <ahmad.labeb@mne.gov>
2018-02-19 14:47:43 +0200

    create bills and items

---
+++ systemBuy/Models/sideBuy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace systemBuy.Models
+{
+    public class sideBuy
+    {
+        public int id { get; set; }
+    }
+}
